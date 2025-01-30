03558f945450758e52bd570cfb6c342db1e7360a
JTR254 <jackrindge@gmail.com>
2025-01-30 16:17:46 -0500

    Completed Task 1 and 2

---
+++ in_class_assignment_1.js
@@ -0,0 +1,22 @@
+// Task 1
+
+let x = 10
+if (x >  5) {
+    console.log ("Help")
+};
+
+let expenses = 10000;
+if (x > 7000) {
+    console.log("High Expenses")
+} else {
+    console.log("Manageable Expenses")
+};
+
+//Task 2
+
+let score = 60
+if (x >= 60) {
+    console.log("Passed")
+} else {
+    console.log("Failed")
+}
